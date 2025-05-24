567918e551cd58e704e02a30912e439bda3af4ed
Completrix <aicmt88@gmail.com>
2025-05-24 23:58:32 +0530

    sd

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -45,6 +45,7 @@ const Footer = () => {
                 <MailIcon className="w-5 h-5 mr-2 flex-shrink-0" />
                 <span>aachmanayurveda@gmail.com</span>
               </p>
+              {/* hello */}
               <a 
                 href="https://wa.me/919211627274" 
                 target="_blank" 
